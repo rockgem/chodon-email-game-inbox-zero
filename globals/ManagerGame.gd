60f977a9d37d21e1b2727cac24b19a402478e039
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2025-04-14 10:12:04 +0800

    fdgd

--- globals/ManagerGame.gd
+++ globals/ManagerGame.gd
@@ -5,10 +5,11 @@ signal pop_to_ui(instance)
 
 
 var email_data = {
-	'from': 'evil@gmail.com',
-	'from_name': 'evil G.',
+	'sender_email': 'evil@gmail.com',
+	'sender_name': 'evil G.',
 	'to': 'rock@gmail.com',
 	'to_name': 'Rock G.',
-	'message': 'hello message',
+	'subject': 'Title Header',
+	'body': 'hello message',
 	'date': '1/1/10'
 }
