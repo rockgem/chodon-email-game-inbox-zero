8d60a7888e9779a6c19ca39b9b5f5febb2643dd4
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2025-04-15 11:01:07 +0800

    dfdf

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -7,7 +7,13 @@ var emails = []
 
 func _ready() -> void:
 	ManagerGame.pop_to_ui.connect(on_pop_to_ui)
+	
+	load_day_emails()
 
+
+
+func load_day_emails():
+	pass
 
 
 func on_pop_to_ui(instance):
